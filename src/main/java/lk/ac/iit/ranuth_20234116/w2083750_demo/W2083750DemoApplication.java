99e8cf9834065bdc2d5e563230486bfc8e709f9b
Ranux123 <ranuthofficial@gmail.com>
2024-11-01 06:31:34 +0530

    01/11: Initialized the project Structure

---
+++ src/main/java/lk/ac/iit/ranuth_20234116/w2083750_demo/W2083750DemoApplication.java
@@ -0,0 +1,15 @@
+package lk.ac.iit.ranuth_20234116.w2083750_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W2083750DemoApplication
+{
+
+    public static void main( String[] args )
+    {
+        SpringApplication.run( W2083750DemoApplication.class, args );
+    }
+
+}
---
+++ src/test/java/lk/ac/iit/ranuth_20234116/w2083750_demo/W2083750DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ac.iit.ranuth_20234116.w2083750_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W2083750DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
